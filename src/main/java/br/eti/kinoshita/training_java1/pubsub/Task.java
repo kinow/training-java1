4dea28b8fab27486aa969a882fb413f2845ee28c
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-12-13 17:08:52 +1300

    Fix compile error and adding a producer

--- src/main/java/br/eti/kinoshita/training_java1/pubsub/Task.java
+++ src/main/java/br/eti/kinoshita/training_java1/pubsub/Task.java
@@ -1,6 +1,10 @@
 package br.eti.kinoshita.training_java1.pubsub;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -12,21 +16,18 @@ public abstract class Task<T> implements Runnable {
 
     private final UUID identifier;
     private final LocalDateTime created;
-    private final LocalDateTime started;
-    private final LocalDateTime finished;
-    private final T result;
+    private LocalDateTime started;
+    private LocalDateTime finished;
+    private T result;
 
     private final Random generator = new Random();
 
     private static final Logger LOGGER = Logger.getLogger(Task.class.getName());
 
-    public Task(UUID identifier, LocalDateTime created, LocalDateTime started, LocalDateTime finished, T result) {
+    public Task() {
         super();
-        this.identifier = identifier;
-        this.created = created;
-        this.started = started;
-        this.finished = finished;
-        this.result = result;
+        this.identifier = UUID.randomUUID();
+        this.created = LocalDateTime.now(Clock.systemUTC());
     }
 
     public UUID getIdentifier() {
@@ -49,22 +50,41 @@ public abstract class Task<T> implements Runnable {
         return result;
     }
 
+    static class NumberTask extends Task<Number> {
+
+    }
+
     /**
-     * A dummy task that simply lasts betwen 500 and 1000 milliseconds. It logs the
-     * current nano time at every execution.
+     * A dummy task that simply lasts betwen 500 and 1000 milliseconds. It logs the current nano time at every
+     * execution.
      */
     public void run() {
         try {
-            LOGGER.info(String.format("%d", System.nanoTime()));
+            started = LocalDateTime.now(Clock.systemUTC());
+            LOGGER.info(String.format("Thread started at %s", started.toString()));
             Thread.sleep(generator.nextInt(500) + 500);
         } catch (InterruptedException e) {
             LOGGER.log(Level.SEVERE, "Task failed: " + e.getMessage(), e);
+        } finally {
+            finished = LocalDateTime.now(Clock.systemUTC());
         }
     }
 
     public static void main(String[] args) {
-        int numberOfTasks = 50;
-        ExecutorService pool = Executors.newFixedThreadPool();
-
+        final int numberOfTasks = 50;
+        final List<Task<?>> tasks = Collections.synchronizedList(new ArrayList<>(numberOfTasks));
+        // 1 producer
+        ExecutorService producer = Executors.newFixedThreadPool(1);
+        producer.submit(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0 ; i < numberOfTasks; i++) {
+                    LOGGER.info("Adding ");
+                    tasks.add(new NumberTask());
+                }
+            }
+        });
+        producer.shutdown();
+        LOGGER.info("OK!");
     }
 }

4b964ba68bc6883fb3b16b0185a88eec90ba1988
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-12-28 18:52:35 +1300

    Add the collector

--- src/main/java/br/eti/kinoshita/training_java1/pubsub/Task.java
+++ src/main/java/br/eti/kinoshita/training_java1/pubsub/Task.java
@@ -3,12 +3,17 @@ package br.eti.kinoshita.training_java1.pubsub;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.LongSummaryStatistics;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -89,6 +94,31 @@ public abstract class Task implements Runnable {
         final int numberOfTasks = 50;
         // buffer supporting 10 tasks only
         final BlockingQueue<Task> tasks = new ArrayBlockingQueue<>(10, /* fair */ false);
+        // the collector job
+        final class CollectorJob {
+            public final LongAdder processed = new LongAdder();
+            public final LongAdder submitted = new LongAdder();
+            public final List<Long> executionTimes = Collections.synchronizedList(new ArrayList<>());
+            
+            public void tallyAndPrintStats() {
+                LongSummaryStatistics stats;
+                synchronized (executionTimes) {
+                    stats = executionTimes.stream().mapToLong((x) -> x).summaryStatistics();
+                }
+                String statsOutput = String.join(System.getProperty("line.separator"),
+                        "",
+                        "##################################################",
+                        "Submitted          " + submitted.sum(),
+                        "Processed          " + processed.sum(),
+                        "Min execution time " + stats.getMin(),
+                        "Max execution time " + stats.getMax(),
+                        "Avg execution time " + stats.getAverage(),
+                        "##################################################"
+                        );
+                LOGGER.info(statsOutput);
+            }
+        };
+        final CollectorJob collector = new CollectorJob();
         // 1 producer
         ExecutorService producer = Executors.newFixedThreadPool(1, new ThreadFactory() {
             @Override
@@ -106,6 +136,7 @@ public abstract class Task implements Runnable {
                         tasks.put(new Task(submitted) {
                         });
                         submitted++;
+                        collector.submitted.increment();
                         if (submitted == numberOfTasks) {
                             LOGGER.info("PRODUCER -- out");
                             break;
@@ -137,6 +168,8 @@ public abstract class Task implements Runnable {
                         LOGGER.info("CONSUMER -- Number task " + task.getIdentifier() + " received. Took: "
                                 + task.getResult());
                         processed++;
+                        collector.processed.increment();
+                        collector.executionTimes.add((Long) task.getResult());
                         if (processed == numberOfTasks) {
                             LOGGER.info("CONSUMER -- out");
                             break;
@@ -149,5 +182,19 @@ public abstract class Task implements Runnable {
         }, "consumer");
         consumer.shutdown();
         LOGGER.info("OK!");
+        // not expecting to have multiple collectors, so submitting just one
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!(producer.isTerminated() && consumer.isTerminated())) {
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                collector.tallyAndPrintStats();
+            }
+        }, "collector-thread").start();
     }
 }
